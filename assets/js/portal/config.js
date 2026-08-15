d462148a432ab18b73028759201e19b99681215d
BALEVIA <Tugasberess@gmail.com>
2026-08-15 12:03:58 +0800

    Delete assets/js/portal directory

--- assets/js/portal/config.js
+++
@@ -1,7 +0,0 @@
-// BALEVA Portal V3
-// Isi hanya Project URL dan Publishable/Anon key.
-// JANGAN menaruh service_role / secret key di browser.
-export const portalConfig = Object.freeze({
-  supabaseUrl: "PASTE_SUPABASE_PROJECT_URL_HERE",
-  supabasePublishableKey: "PASTE_SUPABASE_PUBLISHABLE_KEY_HERE"
-});
